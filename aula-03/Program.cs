73fa78f24d27be0e2f2a7c5685223ab9a45ece48
AndreasAA-Uni9 <alvesaraujoandreas@uni9.edu.br>
2025-03-07 13:47:31 -0300

    Update Program.cs
    Comandos para canais

--- aula-03/Program.cs
+++ aula-03/Program.cs
@@ -35,7 +35,23 @@ Console.WriteLine($"O volume da tv é: {tvSala.Volume}");
 //Deveria imprimir volume 01
 tvSala.AlternarModoMudo();
 
+// ---- Testando os Canais ----
 
+Console.WriteLine($"Canal atual: {tvSala.Canal}");
+tvSala.AumentarCanal();
+Console.WriteLine($"Canal após aumento: {tvSala.Canal}");
+
+tvSala.DiminuirCanal();
+Console.WriteLine($"Canal após diminuição: {tvSala.Canal}");
+
+tvSala.SelecionarCanal(505);
+Console.WriteLine($"Canal selecionado: {tvSala.Canal}");
+
+tvSala.SelecionarCanal(0); // Deve ignorar pois é inválido
+Console.WriteLine($"Canal após tentativa inválida: {tvSala.Canal}");
+
+tvSala.SelecionarCanal(1000); // Deve ignorar pois é maior que o limite
+Console.WriteLine($"Canal após tentativa inválida: {tvSala.Canal}");
 
 
 
